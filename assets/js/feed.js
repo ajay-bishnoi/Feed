cbd8b78dd4a0424cd00e2a9cbf28910213404ff8
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-01-18 10:46:02 +0530

    first commit

---
+++ assets/js/feed.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function openNav() {
+  document.querySelector(".closeNav").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hide");
+  document.querySelector(".menu").classList.toggle("cross");
+}
